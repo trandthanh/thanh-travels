2b3d5369fd927890cedc3bbab08959c3962b8328
Dang-Thanh Tran <trandgthanh@gmail.com>
2018-07-20 10:21:20 +0200

    Finished Footer for Location Show, only on Desktop

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,9 +1,17 @@
 class LocationsController < ApplicationController
 
-
   def show
     @location_url = params[:url]
     @location = Location.find_by(url: @location_url)
+
+    @locations = Location.all
+
+    @location_random = Location.all.where("url != ?", current_location.url).sample.url
   end
 
+  private
+
+  def current_location
+    @current_location ||= Location.find_by(url: params[:url]) if params[:url]
+  end
 end
