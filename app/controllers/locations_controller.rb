a5c3eb01a99f49acc1fd1a805a4dbcb5c7bebec3
Dang-Thanh Tran <trandgthanh@gmail.com>
2018-07-20 19:29:19 +0200

    Succeeded making countries in navbar dynamic

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -7,6 +7,11 @@ class LocationsController < ApplicationController
     @locations = Location.all
 
     @location_random = Location.all.where("url != ?", current_location.url).sample.url
+
+    # Navbar
+    @asia_countries = Country.where("continent_id = '3'")
+    @europe_countries = Country.where("continent_id = '2'")
+    @namerica_countries = Country.where("continent_id = '1'")
   end
 
   private
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,8 @@ class PagesController < ApplicationController
   def home
     # Navbar
     @asia_countries = Country.where("continent_id = '3'")
+    @europe_countries = Country.where("continent_id = '2'")
+    @namerica_countries = Country.where("continent_id = '1'")
 
     # Featured Locations
     @featured_locations = Location.where("featured = true")
