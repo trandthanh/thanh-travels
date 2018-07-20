919097e0dd363f8099b3a5c533bfd9982f65a136
Dang-Thanh Tran <trandgthanh@gmail.com>
2018-07-20 11:38:49 +0200

    In the middle of making navbar work

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
 
   def home
+    # Navbar
+    @asia_countries = Country.where("continent_id = '3'")
+
     # Featured Locations
     @featured_locations = Location.where("featured = true")
   end
